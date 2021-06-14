8780292e2295badaa73ac0445235f07b45276173
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-14 22:09:02 +0200

    extend using the emitter class

--- app.js
+++ app.js
@@ -5,4 +5,21 @@ eventEmitter.on("tutorial", (num1, num2) => {
   console.log(num1 + num2)
 });
 
-eventEmitter.emit("tutorial",1,2);
+eventEmitter.emit("tutorial", 1, 2);
+
+class Person extends EventEmitter{
+  constructor() {
+    super(),
+      this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+}
+
+let pedro = new Person("Pedro");
+pedro.on("name", () => {
+  console.log("my name is" + pedro.name);
+});
+
+pedro.emit("name")
