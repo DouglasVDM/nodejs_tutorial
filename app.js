8fc43f35bb105503269764160c457a4831708e54
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-14 22:04:40 +0200

    eventemitter with parameters in callback function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const tutorial = require("./tutorial")
-console.log(tutorial.sum(1,1));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("tutorial", (num1, num2) => {
+  console.log(num1 + num2)
+});
+
+eventEmitter.emit("tutorial",1,2);
